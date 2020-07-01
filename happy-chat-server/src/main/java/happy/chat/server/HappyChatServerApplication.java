53643be3e8d136528899357592f36313464330a4
Terry <478409784@qq.com>
2020-07-01 21:59:56 +0800

    初始化项目

---
+++ happy-chat-server/src/main/java/happy/chat/server/HappyChatServerApplication.java
@@ -0,0 +1,13 @@
+package happy.chat.server;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HappyChatServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HappyChatServerApplication.class, args);
+    }
+}
