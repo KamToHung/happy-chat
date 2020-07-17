c716ab98d9753b43217e2c3398773adfd4cba3bf
Terry <478409784@qq.com>
2020-07-17 11:22:23 +0800

    修改聊天室

--- happy-chat-server/src/main/java/happy/chat/server/handler/CreateGroupHandler.java
+++ happy-chat-server/src/main/java/happy/chat/server/handler/CreateGroupHandler.java
@@ -1,23 +1,36 @@
 package happy.chat.server.handler;
 
+import com.google.protobuf.ProtocolStringList;
 import happy.chat.common.protobuf.request.RequestBody;
+import happy.chat.server.util.ChannelGroupUtils;
 import io.netty.channel.ChannelHandler;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.SimpleChannelInboundHandler;
+import io.netty.channel.group.ChannelGroup;
+import io.netty.channel.group.DefaultChannelGroup;
+import io.netty.util.concurrent.EventExecutor;
+import io.netty.util.concurrent.GlobalEventExecutor;
 import org.springframework.stereotype.Component;
+
+import java.util.UUID;
 
 /**
  * 创建聊天室
  *
  * @author Terry
- *
  */
 @Component
 @ChannelHandler.Sharable
 public class CreateGroupHandler extends SimpleChannelInboundHandler<RequestBody.RequestMsg.CreateGroup> {
 
     @Override
-    protected void channelRead0(ChannelHandlerContext ctx, RequestBody.RequestMsg.CreateGroup msg) throws Exception {
+    protected void channelRead0(ChannelHandlerContext ctx, RequestBody.RequestMsg.CreateGroup requestBody) throws Exception {
+        //新建channelGroup
+        ChannelGroup channelGroup = new DefaultChannelGroup(GlobalEventExecutor.INSTANCE);
+        String groupId = UUID.randomUUID().toString();
+        ProtocolStringList userIds = requestBody.getUserIdsList();
+        userIds.forEach(userId -> {
 
+        });
     }
 }
